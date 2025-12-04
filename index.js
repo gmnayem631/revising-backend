21e22154fe275520918e1edf175ba8f222f13290
gmnayem631 <gmnayem631@gmail.com>
2025-12-04 18:26:01 +0600

    created basic express server with some hard coded user data

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log("Hello World");
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
+
+const users = [
+  { id: 1, name: "George", email: "george.bailly@gmail.com" },
+  { id: 2, name: "Shane", email: "shane.bond@gmail.com" },
+  { id: 3, name: "Chris", email: "chris.rogers@gmail.com" },
+];
+
+app.get("/users", (req, res) => {
+  res.send(users);
+});
+
+app.listen(port, () => {
+  console.log(`Running from port ${port}`);
+});
